eb87500be6b97be4d402a045f34b136e35de0104
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-05-06 10:54:22 +0530

    CPP

---
+++ Array/1_Find Maximum and Minimum Element Array/Hello.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int findMinumum (int arr[], int size) {
+    int minAns  = INT16_MAX;
+
+    for(int i=0; i<size; i++) {
+        if(arr[i] < minAns) {
+            minAns = arr[i];
+        }
+    }
+    return minAns;
+}
+
+int main() {
+    int arr[7] = {2,0,4,1,40,15,80};
+    int size = 7;
+
+    int minimum = findMinumum(arr, size);
+    cout << "Minimun ans is :- " << minimum << endl;
+}
