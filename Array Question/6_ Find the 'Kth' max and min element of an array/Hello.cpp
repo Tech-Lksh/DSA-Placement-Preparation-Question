255edcfed803a2f3bfc68245bfd86b31deac354e
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-05-10 23:45:49 +0530

    C++

--- Array Question/6_ Find the 'Kth' max and min element of an array/Hello.cpp
+++ Array Question/6_ Find the 'Kth' max and min element of an array/Hello.cpp
@@ -44,3 +44,27 @@ int main()
 
     selectionSort(arr, n);
 }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int findKthMinMax(int arr[], int size) {
+//     int min_Kth = INT16_MAX;
+
+//     for(int i=0; i<size; i++) {
+//         if(arr[i] < min_Kth) {
+//             min_Kth = arr[i];
+//         }
+//     }
+//      return min_Kth;
+// }
+
+
+// int main() {
+//     int arr[] = {7,9,2,4,11,23,1};
+//     int size = 7;
+
+//     int minimum = findKthMinMax(arr, size);
+//     cout << minimum;
+// }
