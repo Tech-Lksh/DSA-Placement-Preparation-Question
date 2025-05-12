3cea9af6adaf2305cdd000cd00a5afc476ac99f8
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-05-12 13:40:10 +0530

    C++

--- Array Question/12_Count All set bits/Hello.cpp
+++ Array Question/12_Count All set bits/Hello.cpp
@@ -1,13 +1,37 @@
 #include<iostream>
 using namespace std;
 
+// 1_Number System Method
+
+// int cuntSetBit(int num) {
+//     int count = 0;
+//     while(num > 0) {
+//         int bit = num % 2;
+//         if(bit == 1)
+//             count++;
+//             num = num/2;
+//     }
+//     cout << "Count is :- " << count;
+// }
+
+// int main () {
+//     int num;
+//     cout << "Enter value of num :- ";
+//     cin >> num;
+
+//     cuntSetBit(num);
+// }
+
+
+// 2_Bitwise Method
+
 int cuntSetBit(int num) {
     int count = 0;
     while(num > 0) {
-        int bit = num % 2;
+        int bit = (num & 1);
         if(bit == 1)
             count++;
-            num = num/2;
+            num = num>>1;
     }
     cout << "Count is :- " << count;
 }
@@ -19,5 +43,3 @@ int main () {
 
     cuntSetBit(num);
 }
-
-
