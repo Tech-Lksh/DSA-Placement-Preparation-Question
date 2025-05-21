90bb1fd8aa8c88fe8c7371a600e0c9052cb2073a
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-05-21 10:27:40 +0530

    C++

--- 3_String Questions/2_Minimise String/Hello.cpp
+++ 3_String Questions/2_Minimise String/Hello.cpp
@@ -5,7 +5,16 @@ using namespace std;
  
 int main()
 {
-    
+    string a = "aaabbccccdddddeeeeee";
+    int count = 1;
+    for(int i =0; i <a.length(); i++) {
+        if(a[i] == a[i+1]) {
+            count++;
+        } else if(a[i] != a[i +1]) {
+            cout << a[i] << count << " ";
+            count = 1;
+        }
+    }
     
     return 0;
 }
---
+++ 3_String Questions/3_Reverse String/Hello.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+ 
+int main()
+{
+    string a = "Lokesh";
+    for(int i =a.length()-1; i >=0; i--) {
+        cout << a[i] << " ";
+    }
+    
+    return 0;
+}
+
