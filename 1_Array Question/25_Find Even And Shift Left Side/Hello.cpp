aa91ba17a5a871030796c63c185377ee41464f53
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-05-26 22:36:05 +0530

    C++

---
+++ 1_Array Question/25_Find Even And Shift Left Side/Hello.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int evenLeft(int arr[], int size) {
+    int ans;
+    for(int i =0; i <size; i++) {
+        if(arr[i]%2 == 0) {
+            
+        } else {
+        }
+    }
+}
+ 
+int main()
+{
+    int arr[] = {24,33,3,46,78,63,69};
+    int size = 7;  
+    
+    evenLeft(arr, size);
+    return 0;
+}
