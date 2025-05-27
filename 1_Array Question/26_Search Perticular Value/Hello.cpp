5dc0d2c458442dad227d54622f97402601d89202
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-05-27 14:28:09 +0530

    C++

---
+++ 1_Array Question/26_Search Perticular Value/Hello.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+ 
+int main()
+{
+    vector<int> arr;
+    int i;
+    for(i=0; i<1000; i++) {
+        arr.push_back(i);
+    // cout << arr[i] << " ";
+    if(arr[i] > 100 && arr[i] < 201) {
+        cout << arr[i] << " ";
+    }
+    }
+    
+    return 0;
+}
