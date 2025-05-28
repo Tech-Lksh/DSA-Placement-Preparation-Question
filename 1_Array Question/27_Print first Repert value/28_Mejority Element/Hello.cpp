3b35ef3e79eb457ce248b2fe636d53bf9eeda878
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-05-28 08:56:37 +0530

    C++

---
+++ 1_Array Question/27_Print first Repert value/28_Mejority Element/Hello.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int findMejorityElmt(int arr[], int size) {
+    sort(arr, arr+size);
+    for(int val=0; val<size; val++) {
+            int freq = 0;
+            for(int element=0; element<size; element++) {
+                if(element == val) {
+                    freq++;
+                }
+            }
+            if(freq > size/2) {
+                return val;
+            }
+        }
+}
+ 
+int main()
+{
+    int arr[] = {3,3,3,3,3,3,3,2,2,2,2,2};
+    int size = 12;
+    
+    findMejorityElmt(arr, size);
+    
+    return 0;
+}
