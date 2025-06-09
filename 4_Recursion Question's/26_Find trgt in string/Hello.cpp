f27a70aaf98673e5eb0efcb40a43331ecbcab45f
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-06-10 01:20:26 +0530

    C++

---
+++ 4_Recursion Question's/26_Find trgt in string/Hello.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<limits.h>
+#include<math.h>
+using namespace std;
+
+// int find(string str, int index, int size, char trgt) {
+//     // Base Case
+//     if(index >= size) {
+//         return -1;
+//     }
+
+//     // Processing
+//     if(str[index] == trgt) {
+//         return index;
+//     }
+
+//     // recursive case
+//     return find(str, index+1, size, trgt);
+// }
+
+int find(string str, int index, int size, char trgt, vector<int> &ans) {
+    // Base Case
+    if(index >= size) {
+        return -1;
+    }
+
+    // Processing
+    if(str[index] == trgt) {
+        ans.push_back(index);
+        // return index;
+    }
+
+    // recursive case
+    return find(str, index+1, size, trgt, ans);
+}
+ 
+int main() {
+    string str = "Lokesh";
+    int index = 0;
+    int size = str.length();
+    char trgt = 'k';
+    vector<int> ans;
+
+    find(str, index, size, trgt, ans);
+
+    for(int num : ans) {
+        cout << "Indx is : " << num << endl;
+    }
+    return 0;
+}
