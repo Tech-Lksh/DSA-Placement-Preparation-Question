267dd9818f2cef41e35ca6da489c29ecb8ff9eb0
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-06-11 17:48:15 +0530

    C++

---
+++ 1_Array Question/30_Merge Two Sorted Array/Hello.cpp
@@ -0,0 +1,38 @@
+// Write a code to merge two sorted array
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<limits.h>
+#include<math.h>
+using namespace std;
+
+void mergeArry(int arr1[], int arr2[], int size1, int size2, vector<int> &ans) {
+    int i = 0;
+    int j = 0;
+    while(i<=size1 && j<size2) {
+        if(arr1[i]<arr2[j]) {
+            ans.push_back(arr1[i]);
+            i++;
+        }
+        else {
+            ans.push_back(arr2[j]);
+            j++;
+        }
+    }
+}
+
+int main() {
+    int arr1[] = {1,3,5,7,9};
+    int arr2[] = {2,4,6,8,10};
+    int size1 = 5;
+    int size2 = 5;
+
+    vector<int> ans;
+
+    mergeArry(arr1, arr2, size1, size2, ans);
+    for(int num : ans) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
