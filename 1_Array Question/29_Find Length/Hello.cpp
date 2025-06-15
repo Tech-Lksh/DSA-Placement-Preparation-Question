79c49e2a54f85aeb60b18056bdf7c1748b5fc447
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-06-16 01:21:36 +0530

    C++

--- 1_Array Question/29_Find Length/Hello.cpp
+++ 1_Array Question/29_Find Length/Hello.cpp
@@ -4,34 +4,34 @@
 #include<limits.h>
 #include<math.h>
 using namespace std;
-
-void find(int arr[], int arr1[], int size, int size1, vector<int> &ans) {
-    for(int i=0; i<size; i++) {
-        for(int j=0; j<size1; j++) {
-            int currIndx = 0;
-            if(arr[i] == arr1[j]) {
-                ans.push_back(arr1[j]);
-                currIndx =j;
-            }
-            j = currIndx+1;
-            if(j >= size1) {
-                j=0;
-            }
+void findLen(string str, int length, string str1, int length1, vector<string> &ans) {
+    
+    int i = 0, j = 0;
+    while(i < length && j < length1) {
+        int index = 0;
+        if(str[i] == str1[j]){
+            // ans.push_back(str1[j]);
+            cout << str1[j] << " ";
+            index = j;
         }
+        else {
+            j = index + 1;
+        }
+        j++;
+        i++;            
+        
+
     }
 }
  
 int main() {
-    int arr[] = {1,2,3,4,5,6,7};
-    int size = 7;
-    int arr1[] = {2,4,5,8,3,1,4};
-    int size1 = 7;
-    vector<int> ans;
+    string str = "ABCDEFGH";
+    int length = str.length();
+    string str1 = "BDGFCJHE";
+    int length1 = str1.length();
+    vector<string> ans;
 
-    find(arr, arr1, size, size1, ans);
-    for(int num : ans) {
-        cout << ans[num];
-    }
+    findLen(str, length, str1, length1, ans);
     
     return 0;
 }
